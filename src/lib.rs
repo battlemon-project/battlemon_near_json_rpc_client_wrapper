202ae5f70237f37ee513b5aaf66cfdf6ae61cac4
ChzenChzen <fedorovdanila@me.com>
2022-04-30 00:05:44 +0800

    add connect and block_height_from_hash methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use near_jsonrpc_client::{methods, AsUrl, JsonRpcClient};
+use near_primitives::hash::CryptoHash;
+use near_primitives::types::{BlockId, BlockReference};
+
+type GenericError = Box<dyn std::error::Error + Sync + Send>;
+type Result<T> = std::result::Result<T, GenericError>;
+
+pub struct JsonRpcWrapper(JsonRpcClient);
+
+impl JsonRpcWrapper {
+    pub fn connect<T: AsUrl>(server_address: T) -> Self {
+        Self(JsonRpcClient::connect(server_address))
+    }
+
+    pub async fn block_height_from_hash(&self, block_hash: CryptoHash) -> Result<u64> {
+        let request = methods::block::RpcBlockRequest {
+            block_reference: BlockReference::BlockId(BlockId::Hash(block_hash)),
+        };
+        let response = self.0.call(request).await?;
+        Ok(response.header.height)
     }
 }
