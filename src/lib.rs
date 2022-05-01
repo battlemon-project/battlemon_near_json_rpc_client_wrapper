07c4395bbf15ba56fc132909a21387aca279038a
ChzenChzen <fedorovdanila@me.com>
2022-05-01 19:32:10 +0800

    add creds field for change methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,28 @@
-use near_jsonrpc_client::{methods, AsUrl, JsonRpcClient};
-use near_primitives::hash::CryptoHash;
-use near_primitives::types::{BlockId, BlockReference};
-
+use near_crypto::{InMemorySigner, SecretKey};
 pub use near_jsonrpc_client::NEAR_MAINNET_ARCHIVAL_RPC_URL;
 pub use near_jsonrpc_client::NEAR_TESTNET_ARCHIVAL_RPC_URL;
+use near_jsonrpc_client::{methods, AsUrl, JsonRpcClient};
+use near_primitives::hash::CryptoHash;
+use near_primitives::types::{AccountId, BlockId, BlockReference, Finality};
 
 type GenericError = Box<dyn std::error::Error + Sync + Send>;
 type Result<T> = std::result::Result<T, GenericError>;
 
-#[derive(Debug)]
-pub struct JsonRpcWrapper(JsonRpcClient);
+pub struct JsonRpcWrapper {
+    client: JsonRpcClient,
+    signer: InMemorySigner,
+}
 
 impl JsonRpcWrapper {
-    pub fn connect<T: AsUrl>(server_address: T) -> Self {
-        Self(JsonRpcClient::connect(server_address))
+    pub fn connect<T: AsUrl>(
+        server_address: T,
+        signer_account_id: AccountId,
+        signer_secret_key: SecretKey,
+    ) -> Self {
+        Self {
+            client: JsonRpcClient::connect(server_address),
+            signer: InMemorySigner::from_secret_key(signer_account_id, signer_secret_key),
+        }
     }
 
     pub async fn block_height_from_hash(&self, block_hash: CryptoHash) -> Result<u64> {
