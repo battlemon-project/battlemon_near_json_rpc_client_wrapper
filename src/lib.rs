a531e287cf7bfbd267247d345c217977187ce852
ChzenChzen <fedorovdanila@me.com>
2022-05-02 12:00:58 +0800

    add call_contract_method

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-use near_crypto::{InMemorySigner, SecretKey};
+use near_crypto::InMemorySigner;
 pub use near_jsonrpc_client::NEAR_MAINNET_ARCHIVAL_RPC_URL;
 pub use near_jsonrpc_client::NEAR_TESTNET_ARCHIVAL_RPC_URL;
 use near_jsonrpc_client::{methods, AsUrl, JsonRpcClient};
+use near_jsonrpc_primitives::types::query::QueryResponseKind;
 use near_primitives::hash::CryptoHash;
-use near_primitives::types::{BlockId, BlockReference, Finality};
+use near_primitives::transaction::Transaction;
+use near_primitives::types::{AccountId, Balance, BlockId, BlockReference, Finality, Gas};
+use std::fmt;
 
 type GenericError = Box<dyn std::error::Error + Sync + Send>;
 type Result<T> = std::result::Result<T, GenericError>;
@@ -37,5 +40,47 @@ impl JsonRpcWrapper {
         let response = self.client.call(request).await?;
 
         Ok(response.header.height)
+    }
+
+    pub async fn call_contract_method<T: serde::Serialize + fmt::Display>(
+        &self,
+        contract_id: AccountId,
+        method: String,
+        json_args: T,
+        gas: Gas,
+        deposit: Balance,
+    ) -> Result<()> {
+        let access_key_query_response = self
+            .client
+            .call(methods::query::RpcQueryRequest {
+                block_reference: BlockReference::latest(),
+                request: near_primitives::views::QueryRequest::ViewAccessKey {
+                    account_id: self.signer.account_id.clone(),
+                    public_key: self.signer.public_key.clone(),
+                },
+            })
+            .await?;
+
+        let current_nonce = match access_key_query_response.kind {
+            QueryResponseKind::AccessKey(access_key) => access_key.nonce,
+            _ => return Err("Failed to extract current nonce".into()),
+        };
+        let args = json_args.to_string().into_bytes();
+        let transaction = Transaction::new(
+            self.signer.account_id.clone(),
+            self.signer.public_key.clone(),
+            contract_id,
+            current_nonce + 1,
+            access_key_query_response.block_hash,
+        )
+        .function_call(method, args, gas, deposit);
+
+        let request = methods::broadcast_tx_commit::RpcBroadcastTxCommitRequest {
+            signed_transaction: transaction.sign(&self.signer),
+        };
+
+        self.client.call(request).await?;
+
+        Ok(())
     }
 }
